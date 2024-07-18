3b27775cbc921aeb0bdebcb74e262fd1a6235c7a
Maciej Bobrowski <mabobrowski@gmail.com>
2024-07-18 16:40:41 +0200

    add counter plugin and carousel plugin

--- src/assets/ts/pages/index.ts
+++ src/assets/ts/pages/index.ts
@@ -3,6 +3,8 @@ import '../../scss/style.scss';
 import '../polyfiles';
 import { domReady } from '../helpers';
 import { initMobile } from '../mobile-nav';
+import { Carousel } from '../plugins/carousel/carousel';
+import { Counter } from '../plugins/counter/counter';
 
 let BODY: HTMLElement | null = null;
 
@@ -20,6 +22,9 @@ window.addEventListener('load', () => {
   BODY.classList.remove('no-animation');
 
   if (BODY) BODY.append(testDynamic());
+
+  initSliders();
+  initCounters();
 });
 
 const buttons = document.querySelectorAll('button');
@@ -64,4 +69,40 @@ const testDynamic = (text = 'Hello world'): HTMLElement => {
 };
 
 
+const initSliders = () => {
+  document.querySelectorAll('[data-carousel]').forEach(carousel => {
+    const animationType = carousel.getAttribute('data-carousel') || 'slide';
+    const interval = carousel.getAttribute('data-interval') || '3000';
 
+    const config = {
+      direction: 'ltr', // 'ltr' lub 'rtl'
+      interval: parseInt(interval), // Czas w milisekundach
+      animationType: animationType, // 'slide' lub 'fade'
+      transitionDuration: '0.5s', // Czas przejścia animacji
+    };
+
+    new Carousel(carousel as HTMLElement, config);
+  });
+};
+
+
+const initCounters = () => {
+  const counters = document.querySelectorAll('[data-counter]');
+  counters.forEach(item => {
+
+    new Counter(item as HTMLElement, {
+      shouldRepeat: true,
+      finishCallback: () => {
+      },
+      preRepeatCallback: () => {
+      },
+
+      targetValue: 4.8,
+      speed: 3000,
+      step: 0.1,
+      easing: 'easeOutQuart',
+      repeatDelay: 2000,
+      initialDelay: 500,
+    });
+  });
+};
